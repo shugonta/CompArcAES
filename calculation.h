d30848af9baadb558c497352ce3d430be9a79c21
shugonta <shugonta@outlook.com>
2017-07-16 19:15:02 +0900

    ブロックサイズ変更

--- calculation.h
+++ calculation.h
@@ -13,10 +13,10 @@
 /*******************************************************************************/
 //#define FILESIZE (32)
 #define FILESIZE (16*128*13*16*512)
-#define BLOCKSIZE (512)
-#define GRIDSIZE_X (128)
-#define GRIDSIZE_Y (13)
-#define GRIDSIZE_Z (16)
+#define BLOCKSIZE (128)
+#define GRIDSIZE_X (128*26*32)
+#define GRIDSIZE_Y (1)
+#define GRIDSIZE_Z (1)
 
 
 void SubBytes(int *);                 /* FIPS 197  P.16 Figure  6 */
