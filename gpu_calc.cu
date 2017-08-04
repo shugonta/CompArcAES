86706cfec4608f62d71210fc697793ef036e56d8
shugonta <shugonta@outlook.com>
2017-08-04 15:43:58 +0900

    printf

--- gpu_calc.cu
+++ gpu_calc.cu
@@ -45,7 +45,12 @@ __global__ void device_aes_encrypt(unsigned char *pt, unsigned char *ct, long in
   unsigned char cb[NBb2];
   int *cw = (int *) cb;
   int *state = (int *) &(pt[thread_id << 4]);
-
+  if (thread_id == 0) {
+    printf("state0: 0x%x\n", ((int *) ct)[thread_id << 2]);
+    printf("state1: 0x%x\n", ((int *) ct)[thread_id << 2|1]);
+    printf("state2: 0x%x\n", ((int *) ct)[thread_id << 2|2]);
+    printf("state3: 0x%x\n", ((int *) ct)[thread_id << 2|3]);
+  }
   cw[0] = state[0] ^ rkey[0];
   cw[1] = state[1] ^ rkey[1];
   cw[2] = state[2] ^ rkey[2];
